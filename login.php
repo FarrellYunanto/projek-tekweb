7e81ce516574367bcdec93b4c5cfb1d661e8ea21
Ezekiel Farrell Yunanto <farrellyunanto@gmail.com>
2023-11-16 10:30:43 +0700

    first commit

---
+++ login.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
+
+    <style>
+        .content {
+            border: 1px solid #eee6e6;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container content center pt-2 pb-2 mt-5 w-50 rounded-4">
+        <div class="row text-center">
+            <div class="col-12">
+                <h2 style="font-weight: bold; color: #03396c;">LOGIN</h2>
+            </div>
+            <div class="col-12">
+                <label for="">Username</label>
+                <input type="text" id="username" class="form-control">
+            </div>
+            <div class="col-12">
+                <label for="">Password</label>
+                <input type="password" id="password" class="form-control">
+            </div>
+
+            <div class="col-12">
+                <button class="btn mt-3" style="font-weight: bold; background-color: #03396c; color: white;">LOGIN</button>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
