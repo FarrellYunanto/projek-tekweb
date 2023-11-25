6b7ce2c38a227ddae22b55dd051b3afded86047a
Ezekiel Farrell Yunanto <farrellyunanto@gmail.com>
2023-11-26 04:24:06 +0700

    fix: register nama & password kebalik

--- process.php
+++ process.php
@@ -6,7 +6,7 @@ if(isset($_POST['processType']) && $_POST['processType'] == 1){
     if(isset($_POST['nrp']) && isset($_POST['nama']) && isset($_POST['password']) && isset($_POST['email'])){
         $sql = "INSERT INTO user (nrp, password, nama, email) VALUES (?,?,?,?)";
         $stmt = $conn->prepare($sql);
-        $stmt->execute([$_POST['nrp'], $_POST['nama'], $_POST['password'], $_POST['email']]);
+        $stmt->execute([$_POST['nrp'], $_POST['password'], $_POST['nama'], $_POST['email']]);
         $success = true;
         $message = "Berhasil membuat akun, silahkan login!";
     } else {
@@ -14,11 +14,17 @@ if(isset($_POST['processType']) && $_POST['processType'] == 1){
     }
 } else if (isset($_POST['processType']) && $_POST['processType'] == 2){
     if(isset($_POST['password']) && isset($_POST['email'])){
-        $sql = "INSERT INTO user (nrp, password, nama, email) VALUES (?,?,?,?)";
-        $stmt = $conn->prepare($sql);
-        $stmt->execute([$_POST['nrp'], $_POST['nama'], $_POST['password'], $_POST['email']]);
-        $success = true;
-        $message = "Berhasil membuat akun, silahkan login!";
+        $stmt = $conn->prepare(
+        "SELECT email, password 
+        FROM `user` 
+        WHERE email = \"" . $_POST['email'] . "\" AND password = \"" . $_POST['password'] . "\"");
+        $stmt->execute();
+
+        if ($stmt->rowCount() > 0) {
+            header("location: ./home/listRuang.php");
+        } else {
+            $message = "Wrong password or email!";
+        }
     } else {
         $message = "Error, data tidak lengkap!";
     }
